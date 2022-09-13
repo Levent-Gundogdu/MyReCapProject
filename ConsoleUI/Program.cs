c282c0a6edd93186423546d68e1e2acc80a80293
leven <levent1061@hotmail.com>
2022-09-13 12:55:13 +0300

    Day 8 Update Part 2

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Business.Concrete;
 using Data_Access.Concrete.InMemory;
 using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
 
 namespace ConsoleUI
 {
@@ -8,13 +9,18 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
+            Car car = new Car();
+            car.Description = "Car";
+            car.DailyPrice = 1;
 
             CarManager carManager = new CarManager(new EfCarDal());
 
-            foreach (var car in carManager.GetCarsByColorId(2))
-            {
-                Console.WriteLine(car.ColorId + " " + car.Description);
-            }
+            //foreach (var car in carManager.GetCarsByColorId(2))
+            //{
+            //    Console.WriteLine(car.ColorId + " " + car.Description);
+            //}
+
+            carManager.Add(car);
 
         }
     }
